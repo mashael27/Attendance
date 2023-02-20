3869fe5acaa8e0ea88555fbb6658a98b46b2a786
hboday <97432007+hboday@users.noreply.github.com>
2023-02-21 01:43:26 +0300

    Delete Attendance-main directory

--- Attendance-main/test/controllers/attendance_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AttendanceControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
-    get attendance_welcome_url
-    assert_response :success
-  end
-end
--- Attendance-main/test/models/shift_assignment_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ShiftAssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
