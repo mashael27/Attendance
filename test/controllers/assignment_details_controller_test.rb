ead150ede049cdc6dc0cc0b05d698a1759d5765d
hboday <97432007+hboday@users.noreply.github.com>
2023-02-21 01:52:56 +0300

    Delete test directory

--- test/controllers/assignment_details_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AssignmentDetailsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get assignment_details_index_url
-    assert_response :success
-  end
-end
